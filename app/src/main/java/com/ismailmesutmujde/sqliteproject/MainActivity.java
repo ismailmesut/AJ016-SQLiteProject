fc790d0870d490f9faa2861e5dc7a037fd139bd9
ismailmesut <ismailmesutmujde@gmail.com>
2024-08-26 16:55:05 +0300

    updated: sqlite database

--- app/src/main/java/com/ismailmesutmujde/sqliteproject/MainActivity.java
+++ app/src/main/java/com/ismailmesutmujde/sqliteproject/MainActivity.java
@@ -1,24 +1,26 @@
 package com.ismailmesutmujde.sqliteproject;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // SQLiteDatabase
+
+        try {
+            SQLiteDatabase database = this.openOrCreateDatabase("Musicians", MODE_PRIVATE, null);
+            database.execSQL("CREATE TABLE IF NOT EXISTS musicians(name VARCHAR, age INT)");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
