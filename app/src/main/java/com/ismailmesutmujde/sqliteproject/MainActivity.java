64dbe373e58ef779f64e9863c09f7a329b1ae0b4
ismailmesut <ismailmesutmujde@gmail.com>
2024-08-26 17:06:34 +0300

    updated: sqlite database - select and insert

--- app/src/main/java/com/ismailmesutmujde/sqliteproject/MainActivity.java
+++ app/src/main/java/com/ismailmesutmujde/sqliteproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ismailmesutmujde.sqliteproject;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
@@ -18,6 +19,18 @@ public class MainActivity extends AppCompatActivity {
         try {
             SQLiteDatabase database = this.openOrCreateDatabase("Musicians", MODE_PRIVATE, null);
             database.execSQL("CREATE TABLE IF NOT EXISTS musicians(name VARCHAR, age INT)");
+
+            //database.execSQL("INSERT INTO musicians (name, age) VALUES ('James', 50)");
+            //database.execSQL("INSERT INTO musicians (name, age) VALUES ('Lars', 60)");
+            Cursor cursor = database.rawQuery("SELECT * FROM musicians", null);
+            int nameIx = cursor.getColumnIndex("name");
+            int ageIx = cursor.getColumnIndex("age");
+
+            while (cursor.moveToNext()) {
+                System.out.println("Name : " + cursor.getString(nameIx));
+                System.out.println("Age : " + cursor.getInt(ageIx));
+            }
+            cursor.close();
 
         } catch (Exception e) {
             e.printStackTrace();
